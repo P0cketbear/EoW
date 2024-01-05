b95a77bc89e6fc408d7b38b0ebe6f8db0436b350
P0cketbear <102195108+P0cketbear@users.noreply.github.com>
2024-01-05 19:31:14 -0300

    rebuilt ui using dom manipulations & events

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ function playRound(playerSelect, cpuSelect) {
 
     switch (playerSelect) {
         case "Axe":
-            if(cpuSelect === "Axe"){
+            if (cpuSelect === "Axe") {
                 return respuesta = "Tie";
             };
             if (cpuSelect === "Bow") {
@@ -20,7 +20,7 @@ function playRound(playerSelect, cpuSelect) {
             }
             break;
         case "Bow":
-            if(cpuSelect === "Axe"){
+            if (cpuSelect === "Axe") {
                 return respuesta = "Win";
             };
             if (cpuSelect === "Bow") {
@@ -32,7 +32,7 @@ function playRound(playerSelect, cpuSelect) {
             break;
 
         case "Wand":
-            if(cpuSelect === "Axe"){
+            if (cpuSelect === "Axe") {
                 return respuesta = "Lose";
             };
             if (cpuSelect === "Bow") {
@@ -40,35 +40,118 @@ function playRound(playerSelect, cpuSelect) {
             }
             if (cpuSelect === "Wand") {
                 return respuesta = "Tie";
-            }            
+            }
             break;
     }
-    
-    
+
+
 }
+
+
+// Buttons
+
+const axebtn = document.querySelector('#axe-btn');
+axebtn.addEventListener('click', () => game('Axe'));
+const wandbtn = document.querySelector('#wand-btn');
+wandbtn.addEventListener('click', () => game('Wand'));
+const bowbtn = document.querySelector('#bow-btn');
+bowbtn.addEventListener('click', () => game('Bow'));
+
+
+// Game
 
 let playerScore = 0;
 let cpuScore = 0;
 
 function game(weapon) {
-    let cpuSelect = getComputerChoice();
-    let playerSelect = weapon;
 
-    let resultado = playRound(playerSelect, cpuSelect);
+    let cpuWeapon = getComputerChoice();
 
-    document.getElementById('resultado').innerHTML = resultado;
+    showPicks(weapon, cpuWeapon);
+
+    let resultado = playRound(weapon, cpuWeapon);
 
     switch (resultado) {
         case 'Win':
             playerScore++;
-            document.getElementById('player').innerHTML = playerScore;
+
+            document.querySelector('#resultado').style.color = 'green';
+            document.querySelector('#resultado').textContent = 'You' + ' ' + resultado + ' this round.';
+
+            document.querySelector('#cpu').style.color = 'white';
+
+            document.querySelector('#player').style.color = 'green';
+            document.querySelector('#player').textContent = playerScore;
             break;
         case 'Lose':
             cpuScore++;
-            document.getElementById('cpu').innerHTML = cpuScore;
+            document.querySelector('#resultado').style.color = 'red';
+            document.querySelector('#resultado').textContent = 'You' + ' ' + resultado + ' this round.';
+
+            document.querySelector('#player').style.color = 'white';
+
+            document.querySelector('#cpu').style.color = 'green';
+            document.querySelector('#cpu').textContent = cpuScore;
+            break;
+        case 'Tie':
+            document.querySelector('#resultado').style.color = 'white';
+            document.querySelector('#cpu').style.color = 'white';
+            document.querySelector('#player').style.color = 'white';
+            document.querySelector('#resultado').textContent = 'It\'s a ' + resultado;
             break;
     }
+
+    checkScore(playerScore, cpuScore);
 }
 
 
+function showPicks(playerWeapon, cpuWeapon) {
 
+
+    const cWeapon = document.querySelector('#cpuWeapon');
+    const pWeapon = document.querySelector('#playerWeapon');
+    const pick1 = document.querySelector('#pick1');
+    const pick2 = document.querySelector('#pick2');
+
+    if (pick1 && pick2) {
+
+        pick1.textContent = 'You picked ' + playerWeapon;
+        pick2.textContent = 'CPU picked ' + cpuWeapon;
+
+    } else {
+        const newPicks = document.createElement('div');
+        newPicks.classList.toggle('picks');
+        newPicks.setAttribute('id', 'pick1')
+        newPicks.textContent = 'You picked ' + playerWeapon;
+        pWeapon.appendChild(newPicks);
+
+        const newPicks2 = document.createElement('div');
+        newPicks2.classList.toggle('picks');
+        newPicks2.setAttribute('id', 'pick2');
+        newPicks2.textContent = 'CPU picked ' + cpuWeapon;
+        cWeapon.appendChild(newPicks2);
+    }
+
+}
+
+function checkScore(playerScore, cpuScore) {
+
+    if (playerScore === 5 || cpuScore === 5) {
+        let winner = '';
+        if (playerScore === 5) {
+            winner = 'Player';
+        } else {
+            winner = 'CPU';
+        }
+
+        const confirmation = confirm(winner + ' wins! Final score - Player: ' + playerScore + ', CPU: ' + cpuScore + ' Click OK to play again.');
+
+        if (confirmation) {
+
+            window.location.reload();
+        } else {
+            window.location.reload();
+        }
+    }
+
+}
